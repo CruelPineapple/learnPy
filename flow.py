1ef3a0ae75ab38addf89fbd2eba4d9bbd10e796e
exusiai <1617218599@qq.com>
2020-11-19 17:05:38 +0800

    if and for loop

--- flow.py
+++ flow.py
@@ -1,5 +1,6 @@
-#一个斐波那契数列的例子
-a,b=0,1
+# 一个斐波那契数列的例子
+a, b = 0, 1
+
 # 这是多重赋值
 while a<10:
     print(a,end=',')
@@ -9,3 +10,23 @@ while a<10:
 # 通过缩进来控制循环体范围,结束标志的一个空行
 # 关于print函数的参数,除了要被打印的东西,还接受一些用于格式化的内容
 # 比如上面的第二个参数end,每次打印完打一个逗号
+
+x=int(input("Please enter an interger: "))
+if x<0:
+    x=0
+    print('Negative changed to zero')
+elif x==0:
+    print('Zero')
+elif x==1:
+    print('Single')
+else:
+    print('More')
+
+# else if 在这里是elif,注意冒号和while句用法类似
+# 官方说法,if,elif结构可以看作其他语言中switch case的替代
+
+words=['cat','window','defenestrate']
+for w in words:
+    print(w,len(w))
+
+# 这不就是java的增强for循环吗,js好像也有类似的
