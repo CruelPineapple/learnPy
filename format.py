e13b204da0fef241db1400387f520c6b748b897c
exusiai <1617218599@qq.com>
2020-12-03 20:56:24 +0800

    basic format

---
+++ format.py
@@ -0,0 +1,10 @@
+year = 2020
+event = 'President election'
+print(f'Result of the {year} {event}')
+# 格式化变量字面值
+
+yes_votes = 42_572_654
+no_votes = 43_132_495
+persentage = yes_votes / (no_votes + yes_votes)
+print('{:-9} YES voates {:2.3%}'.format(yes_votes, persentage))
+
