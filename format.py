fb79c4e7a95a92161592a04928bb79b8e88c5b14
exusiai <1617218599@qq.com>
2020-12-03 20:56:40 +0800

    usage of f:

--- format.py
+++ format.py
@@ -8,3 +8,5 @@ no_votes = 43_132_495
 persentage = yes_votes / (no_votes + yes_votes)
 print('{:-9} YES voates {:2.3%}'.format(yes_votes, persentage))
 
+# f格式化中, 变量名后使用:跟一个整数即可限制最小字符宽度
+print(f'{year:10}')
