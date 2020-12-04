2734dc4d0a31886d0da42b9ff895c51439e01b50
exusiai <1617218599@qq.com>
2020-12-04 22:52:27 +0800

    about constructor

--- class.py
+++ class.py
@@ -9,3 +9,13 @@ class MyClass:
 x = MyClass()
 # 类定义结尾需要两个空行
 print(x.data)
+# 构造函数中的self参数是不会被访问的, 例如一个带参数的构造函数:
+
+
+class MyClass2:
+    def __init__(self, num):
+        self.data = num
+
+
+y = MyClass2(2)
+print(y.data)
